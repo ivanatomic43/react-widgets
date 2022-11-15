584b75186d49038947fc3c5fc148ef8a453a8826
ivanatomic43 <ivana.tomic96@gmail.com>
2022-11-15 18:08:44 +0100

    React widgets app

---
+++ src/components/Accordion.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+const Accordion = ({items}) => {
+
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const onTitleClick = (index) => {
+        setActiveIndex(index);
+    };
+
+    const renderedItems = items.map((item, index) => {
+
+        const active = index === activeIndex ? "active" : ""; 
+        return (
+            <React.Fragment key={item.title}>
+                <div 
+                  className={` ${active} title`}
+                  onClick={()=> onTitleClick(index)}
+                >
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className={`${active} content`}>
+                    <p>{item.content}</p>
+                </div>
+            </React.Fragment>
+        );
+    });
+
+    return (
+        <div className="accordion ui styled">
+            {renderedItems}
+        </div>
+    ); 
+};
+
+export default Accordion;
